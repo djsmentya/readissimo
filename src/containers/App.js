5db676a70cbe39827a30a18b20032559b848305f
Eugene Smentyna <djsmentya@192.168.1.2>
2016-09-27 11:20:15 +0300

    Create projects page

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,65 +1,34 @@
 import React, { Component, PropTypes } from 'react'
-import { connect } from 'react-redux'
-import { browserHistory } from 'react-router'
-import Explore from '../components/Explore'
-import { resetErrorMessage } from '../actions'
+import { Col } from 'react-bootstrap'
 
 class App extends Component {
   static propTypes = {
-    // Injected by React Redux
-    errorMessage: PropTypes.string,
-    resetErrorMessage: PropTypes.func.isRequired,
-    inputValue: PropTypes.string.isRequired,
     // Injected by React Router
-    children: PropTypes.node
-  }
-
-  handleDismissClick = e => {
-    this.props.resetErrorMessage()
-    e.preventDefault()
-  }
-
-  handleChange = nextValue => {
-    browserHistory.push(`/${nextValue}`)
-  }
-
-  renderErrorMessage() {
-    const { errorMessage } = this.props
-    if (!errorMessage) {
-      return null
-    }
-
-    return (
-      <p style={{ backgroundColor: '#e99', padding: 10 }}>
-        <b>{errorMessage}</b>
-        {' '}
-        (<a href="#"
-            onClick={this.handleDismissClick}>
-          Dismiss
-        </a>)
-      </p>
-    )
+    children: PropTypes.node,
+    projects: PropTypes.array.isRequired
   }
 
   render() {
-    const { children, inputValue } = this.props
+    const projects = [1,2,3,4,5,6,7].map((project)=>{
+      return (
+        <Col sm={4} key={project}>
+        <div className="well well-sm">
+            <h1>Hello{project}</h1>
+          </div>
+        </Col>
+      )
+    })
     return (
-      <div>
-        <Explore value={inputValue}
-                 onChange={this.handleChange} />
-        <hr />
-        {this.renderErrorMessage()}
-        {children}
+      <div className="container-fluid">
+        <h1>Projects</h1>
+        <hr/>
+        {projects}
       </div>
     )
   }
 }
 
-const mapStateToProps = (state, ownProps) => ({
-  errorMessage: state.errorMessage,
-  inputValue: ownProps.location.pathname.substring(1)
-})
-
-export default connect(mapStateToProps, {
-  resetErrorMessage
-})(App)
+App.defaultProps = {
+  projects: []
+}
+export default App

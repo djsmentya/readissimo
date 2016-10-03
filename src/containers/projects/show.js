066832e2e51c525d7002b92c480f69edefb9d0ae
Eugene Smentyna <djsmentya@gmail.com>
2016-10-03 16:41:27 +0300

    Change board views

--- src/containers/projects/show.js
+++ src/containers/projects/show.js
@@ -3,22 +3,60 @@ import { Grid, Col } from 'react-bootstrap';
 
 
 export default class Dashboard extends Component {
-    render () {
-      let boards = [
-        {name: 'Backlog',stories: [{title: "Test stories", id: 1}]},
-        {name: 'In Progress',stories: []},
-        {name: 'Code Review',stories: []},
-        {name: 'In QA',stories: []},
-        {name: 'Ready to Deploy',stories: []},
-        {name: 'Testing on Staging',stories: []}
+  constructor(props) {
+    super(props);
+    this.state = {boardGroupField: 'stage'};
+  }
+
+  render () {
+      let tasks = [
+        {
+          id: 2,
+          status: 'New',
+          stage: 'Backlog',
+          assigned: [{name: 'Eugene'},{name: 'George'}],
+          title: 'Test task'
+        },
+        {
+          id: 1,
+          status: 'Accepted',
+          stage: 'TODO',
+          assigned: [{name: 'Eugene'},{name: 'George'}],
+          title: 'Test task 2'
+        },
+        {
+          id: 3,
+          status: 'Done',
+          stage: 'QA',
+          assigned: [{name: 'Eugene'},{name: 'George'}],
+          title: 'Test task 2'
+        },
+        {
+          id: 4,
+          status: 'Done',
+          stage: 'Code Review',
+          assigned: [{name: 'Eugene'},{name: 'George'}],
+          title: 'Test task 2'
+        }
       ]
+
+      let grouppedTasks = this.groupTasks(tasks, this.state.boardGroupField)
+
       return (
         <Grid bsClass="container-fluid">
         <h1>Projects</h1>
         <hr/>
-        {boards.map((item)=>{ return <Board key={item.name} name={item.name} stories={item.stories} /> })}
+        {Object.keys(grouppedTasks).map((item)=>{ return <Board key={item} title={item} stories={grouppedTasks[item]} /> })}
         </Grid>
       )
+    }
+
+    groupTasks (tasks, field) {
+      let list = {}
+      tasks.forEach((task)=>{
+        list[task[field]] = (list[task[field]] || []).concat(task)
+      })
+      return list
     }
 }
 
@@ -26,7 +64,7 @@ class Board extends Component {
   render() {
     return(
       <Col sm={3}>
-        <h5>{this.props.name}</h5>
+        <h5>{this.props.title}</h5>
         {this.props.stories.map((story) =>{ return <Story key={story.id} title={story.title} />})}
       </Col>
     )
